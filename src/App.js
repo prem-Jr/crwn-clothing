d1ab6f9bcf99f74938897ad6f69f906e3727b1dc
Rohan <rohan10hulk@gmal.com>
2019-11-18 10:32:06 +0530

    first commitgit add --all

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+import './homepage-component'
+import HomePage from './homepage-component';
+
+function App() {
+  return (
+   <div>
+      <HomePage/>
+   </div>
+  );
+}
+
+export default App;
